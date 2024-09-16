ea998e5bbc203b879d912c02f78abbb48edb858a
jcmlumacad1 <44424424+jcmlumacad1@users.noreply.github.com>
2024-09-16 16:50:38 +0800

    implemented pictures service

--- backend/routes.py
+++ backend/routes.py
@@ -35,7 +35,7 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    return jsonify(data), 200
 
 ######################################################################
 # GET A PICTURE
@@ -44,7 +44,10 @@ def get_pictures():
 
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
+    for picture in data:
+        if picture['id'] == id:
+            return picture, 200
+    abort(404)
 
 
 ######################################################################
@@ -52,7 +55,14 @@ def get_picture_by_id(id):
 ######################################################################
 @app.route("/picture", methods=["POST"])
 def create_picture():
-    pass
+    new_picture = request.get_json()
+    id = new_picture['id']
+    print(new_picture)
+    for picture in data:
+        if picture['id'] == id:
+            return {"Message": f"picture with id {picture['id']} already present"}, 302
+    data.append(new_picture)
+    return new_picture, 201
 
 ######################################################################
 # UPDATE A PICTURE
@@ -61,11 +71,21 @@ def create_picture():
 
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
-    pass
+    new_picture = request.get_json()
+    for picture in data:
+        if picture['id'] == id:
+            for key in new_picture:
+                picture[key] = new_picture[key]
+            return picture, 200
+    return {"message": "picture not found"}, 404
 
 ######################################################################
 # DELETE A PICTURE
 ######################################################################
 @app.route("/picture/<int:id>", methods=["DELETE"])
 def delete_picture(id):
-    pass
+    for i, picture in enumerate(data):
+        if picture['id'] == id:
+            del data[i]
+            return jsonify(), 204
+    return {"message": "picture not found"}, 404
